98763fb526ef9621179af4fd3c6d82344c278748
Iago Tito <iago.oliveira@ccc.ufcg.edu.br>
2019-09-23 09:51:45 -0300

    Começo do roteiro 5

--- roteiros/roteiro5-iago-118110162.sql
+++ roteiros/roteiro5-iago-118110162.sql
@@ -1,1 +1,48 @@
 -- Q1
+SELECT COUNT (e)
+FROM employee AS e
+WHERE e.sex = 'F';
+
+-- Q2
+SELECT AVG (e.salary)
+FROM employee AS e 
+WHERE e.sex = 'M' AND e.address LIKE '%TX';
+
+-- Q3
+SELECT e.superssn AS ssn_supervisor, COUNT (e.ssn) AS qtd_supervisionados
+FROM employee AS e 
+GROUP BY e.superssn
+ORDER BY qtd_supervisionados;
+
+-- Q4
+SELECT s.fname AS nome_supervisor, COUNT(*) AS qtd_supervisionados
+FROM employee AS s INNER JOIN employee AS e ON s.ssn = e.superssn
+GROUP BY s.fname
+ORDER BY qtd_supervisionados;
+
+-- Q5
+SELECT s.fname AS nome_supervisor, COUNT(*) AS qtd_supervisionados
+FROM employee AS s RIGHT OUTER JOIN employee AS e ON s.ssn = e.superssn
+GROUP BY s.fname
+ORDER BY qtd_supervisionados;
+
+-- Q6
+SELECT MIN (qtd) AS qtd
+FROM (SELECT COUNT (w.essn) AS qtd
+      FROM works_on AS w 
+      GROUP BY w.pno) AS qtds;
+
+-- Q7
+
+
+-- Q8
+SELECT w.pno AS num_proj, AVG (e.salary) AS media_sal
+FROM employee AS e, works_on AS w 
+WHERE w.essn = e.ssn
+GROUP BY num_proj;
+
+-- Q9
+SELECT w.pno AS num_proj, p.pname AS proj_nome, AVG (e.salary) AS media_sal
+FROM employee AS e, works_on AS w, project AS p
+WHERE w.essn = e.ssn AND p.pnumber = w.pno
+GROUP BY num_proj, proj_nome;
